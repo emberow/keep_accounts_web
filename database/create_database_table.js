76bf17356dd1fbba85ac352ff54654a8d9b5e3a6
Emberow Lin/WHQ/Wistron <emberow_lin@wistron.com>
2022-05-31 14:52:10 +0800

    test

--- database/create_database_table.js
+++ database/create_database_table.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const database_name = "keep_accounts_database";
 const mysql = require('mysql');
 
@@ -39,45 +39,3 @@ function create_table(con){
             });
       });
 }
-=======
-const database_name = "keep_accounts_database";
-const mysql = require('mysql');
-
-var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-});
-create_database(database_name,connection);
-connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database: database_name
-});
-create_table(connection);
-function create_database(db_name, con){
-    sql = `CREATE DATABASE ${db_name} DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;`
-    con.query(sql, function (err, result) {
-        console.log("database created");
-        });
-
-}
-
-function create_table(con){
-    con.connect(function(err) {
-        if (err) throw err;
-        console.log("Connected!");
-        
-        var sql = "CREATE TABLE expense_income_record (id INT AUTO_INCREMENT PRIMARY KEY, account VARCHAR(20), item varchar(20), price int(10), date decimal(20,0),	state varchar(10))";
-        con.query(sql, function (err, result) {
-            console.log("Table created");
-            });
-
-        sql = "CREATE TABLE account (name VARCHAR(20), password	varchar(20))";
-        con.query(sql, function (err, result) {
-            console.log("Table created");
-            });
-      });
-}
->>>>>>> 807acb856e1fed5776fece5f1c2c6e82f8b2a3d3
